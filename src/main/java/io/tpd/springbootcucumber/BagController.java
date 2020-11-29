11a91389a3ea2b976a5c873ae0c3d716eec91938
hendra.saputra <hendra.saputra@workday.com>
2020-11-29 17:52:20 +0000

    Add logging to controller

--- src/main/java/io/tpd/springbootcucumber/BagController.java
+++ src/main/java/io/tpd/springbootcucumber/BagController.java
@@ -1,5 +1,8 @@
 package io.tpd.springbootcucumber;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/things")
 public final class BagController {
 
+    private final Logger logger = LoggerFactory.getLogger(BagController.class);
     private final Bag bag = new Bag();
 
     @GetMapping
@@ -18,10 +22,12 @@ public final class BagController {
     @ResponseStatus(HttpStatus.CREATED)
     public void addThing(@RequestBody final String something) {
         bag.add(something);
+        logger.info("Added item \"{}\" to the bag", something);
     }
 
     @DeleteMapping
     public void removeEverything() {
         bag.removeEverything();
+        logger.info("Bag is emptied");
     }
 }
